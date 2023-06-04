f6f771fe03a832e1a820770b0c15e278e7db01df
sonthb3992 <sonthb@avnaudit.vn>
2023-06-04 14:42:51 +0700

    update UI

--- src/domain/user.ts
+++ src/domain/user.ts
@@ -7,46 +7,77 @@ import {
   where,
   setDoc,
   doc,
+  updateDoc,
+  getDoc,
 } from 'firebase/firestore';
-import { app } from './firebase';
+import { app, auth } from './firebase';
+import { User, signOut } from 'firebase/auth';
 
-interface User {
-  uid: string;
-  address: string[];
+export async function CreateUserDataToFirebase(
+  user: User,
+  name?: string,
+  address?: string,
+  phone?: string,
+  role?: string
+): Promise<string> {
+  const db = getFirestore(app);
+  const userRef = doc(collection(db, 'users'), user.uid);
+  await setDoc(userRef, {
+    address: address ?? '',
+    name: name ?? '',
+    phone: phone ?? '',
+    role: role ?? 'customer',
+  });
+  return 'success';
 }
 
-function UserFromFirestore(snapshot: DocumentSnapshot<any>): User {
-  const data = snapshot.data();
-  return {
-    uid: data?.uid,
-    address: data?.address,
-  };
-}
-
-function UserToFirestore(option: User): any {
-  return {
-    uid: option.uid,
-    address: option.address,
-  };
-}
-
-async function UserPushToFirebase(user: User): Promise<string> {
+export async function UpdateUserDataToFirebase(
+  user: User,
+  name?: string,
+  address?: string,
+  phone?: string,
+  role?: string
+): Promise<string> {
   try {
     const db = getFirestore(app);
-    const menuOptionRef = collection(db, 'size_options');
-
-    // Create a query against the collection.
-    const q = query(menuOptionRef, where('nameEn', '==', user?.uid));
-    const querySnapshot = await getDocs(q);
-
-    if (user == null) return 'Null menu option.';
-
-    await setDoc(doc(db, 'size_options'), UserToFirestore(user));
+    const userRef = doc(collection(db, 'users'), user.uid);
+    await updateDoc(userRef, {
+      address: address ?? '',
+      name: name ?? '',
+      phone: phone ?? '',
+      role: role ?? 'customer',
+    });
     return 'success';
-  } catch (e) {
-    return 'Error adding size option to Firestore: ' + e;
+  } catch (error) {
+    console.log(error);
+    return 'Update user information failed.';
   }
 }
 
-export type { User };
-export default UserPushToFirebase;
+export async function GetUserRole(userId: string): Promise<string | null> {
+  try {
+    console.log('Getting user role');
+    const db = getFirestore(app);
+    const userRef = doc(collection(db, 'users'), userId);
+    const userDoc = await getDoc(userRef);
+    console.log(userDoc.data());
+    currentRole = userDoc.data()?.role;
+    return userDoc.data()?.role;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export var currentRole: string;
+
+export async function signUserOut() {
+  try {
+    await signOut(auth);
+    // User signed out successfully
+    console.log('User signed out');
+  } catch (error) {
+    // An error occurred during sign out
+    console.error('Error signing out:', error);
+  }
+}
