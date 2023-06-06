7d4041f16e76a400353219b172a4a0ba6f89e73f
sonthb3992 <sonthb@avnaudit.vn>
2023-06-06 14:50:42 +0700

    Change the fetching logic to reduce firebase reads

--- src/domain/menu_option.ts
+++ src/domain/menu_option.ts
@@ -10,6 +10,7 @@ import {
   where,
   setDoc,
   doc,
+  onSnapshot,
 } from 'firebase/firestore';
 import { OptionBase } from './base_option';
 import { app } from './firebase';
@@ -55,6 +56,8 @@ class MenuOption extends OptionBase {
     this.availableSizes = availableSizes || [];
     this.availableToppings = availableToppings || [];
   }
+
+  static allMenuOptions: MenuOption[];
 
   static fromFirestore(
     snapshot: DocumentSnapshot<any>,
@@ -111,7 +114,7 @@ class MenuOption extends OptionBase {
       availableSizes: option.availableSizes,
       availableToppings: option.availableToppings,
       likeCount: option.likeCount,
-      dislikeCount: option.dislikeCount
+      dislikeCount: option.dislikeCount,
     };
   }
 
@@ -154,24 +157,37 @@ class MenuOption extends OptionBase {
     }
   }
 
-
   static async getAll(): Promise<MenuOption[]> {
     try {
+      if (MenuOption.allMenuOptions && MenuOption.allMenuOptions.length > 0) {
+        return MenuOption.allMenuOptions;
+      }
+
       const db = getFirestore(app);
       const menuOptionRef = collection(db, 'menu_options');
+      const q = query(menuOptionRef);
 
-      // Create a query against the collection.
-      const querySnapshot = await getDocs(menuOptionRef);
+      const querySnapshot = await getDocs(q);
 
       if (querySnapshot.empty) {
+        console.log('Query snapshot is empty');
         return [];
       }
 
-      var result = querySnapshot.docs.map((m) =>
-        MenuOption.fromFirestore(m, undefined)
+      const menuOptions = querySnapshot.docs.map((doc) =>
+        MenuOption.fromFirestore(doc, undefined)
       );
 
-      return result;
+      const unsubscribe = onSnapshot(q, (updatedSnapshot) => {
+        const updatedMenuOptions = updatedSnapshot.docs.map((doc) =>
+          MenuOption.fromFirestore(doc, undefined)
+        );
+        MenuOption.allMenuOptions = updatedMenuOptions;
+      });
+
+      MenuOption.allMenuOptions = menuOptions;
+
+      return menuOptions;
     } catch (e) {
       console.log(e);
       return [];
